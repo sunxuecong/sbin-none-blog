3c57e2eec6a5bddaa4937bb89fb9abe4e99a1bef
sunxuecong <sunxc0713@163.com>
2024-08-02 16:55:06 +0800

    feat: first-commit

---
+++ docs/.vitepress/theme/components/MNavLinks.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { slugify } from '@mdit-vue/shared'
+
+import MNavLink from './MNavLink.vue'
+import type { NavLink } from '../types'
+
+const props = defineProps<{
+  tag?: string
+  title: string
+  noIcon?: boolean
+  items: NavLink[]
+}>()
+
+const component = computed(() => props.tag ?? 'h2')
+
+const formatTitle = computed(() => {
+  return slugify(props.title)
+})
+</script>
+
+<template>
+  <component :is="component" v-if="title" :id="formatTitle" tabindex="-1">
+    {{ title }}
+    <a class="header-anchor" :href="`#${formatTitle}`" aria-hidden="true"></a>
+  </component>
+  <div class="m-nav-links">
+    <MNavLink
+      v-for="item in items"
+      :key="item.link"
+      :noIcon="noIcon"
+      v-bind="{ ...item, ...$attrs }"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.m-nav-links {
+  --m-nav-gap: 10px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(130px, 1fr));
+  grid-row-gap: var(--m-nav-gap);
+  grid-column-gap: var(--m-nav-gap);
+  grid-auto-flow: row dense;
+  justify-content: center;
+  margin-top: var(--m-nav-gap);
+}
+
+@each $media, $size in (500px: 140px, 640px: 155px, 768px: 175px, 960px: 200px, 1440px: 240px) {
+  @media (min-width: $media) {
+    .m-nav-links {
+      grid-template-columns: repeat(auto-fill, minmax($size, 1fr));
+    }
+  }
+}
+
+@media (min-width: 960px) {
+  .m-nav-links {
+    --m-nav-gap: 20px;
+  }
+}
+</style>
